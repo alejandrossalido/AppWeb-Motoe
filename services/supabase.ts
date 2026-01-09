cf465f642cb929e69bae02603ef9bcf8c8bfb9a0
alejandrossalido <alejandrosalidojimenez@gmail.com>
2026-01-09 03:19:05 +0100

    Hardcode credentials to debug Vercel Env

--- services/supabase.ts
+++ services/supabase.ts
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-    throw new Error('Missing Supabase environment variables');
-}
+const supabaseUrl = 'https://qijzycmrtiwqvvrfoahx.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpanp5Y21ydGl3cXZ2cmZvYWh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc5MDI4MDcsImV4cCI6MjA4MzQ3ODgwN30.H0W4jZ4OEJ5G6YHkj4ynWaUUIN5DJowO_R85fyc5sPk';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
